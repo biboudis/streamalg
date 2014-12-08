c97bb1c6954fe3c99e4faf9fef7497e7b8898053
Aggelos Biboudis <biboudis@gmail.com>
2014-12-08 03:19:55 +0200

    Add a GADT comment from Haskell.

--- src/main/java/gadt/Visitor.java
+++ src/main/java/gadt/Visitor.java
@@ -3,6 +3,16 @@ package gadt;
 /**
  * Created by bibou on 12/5/14.
  */
+
+// In Haskell with GADTs
+
+//  data Expr a where
+//      IntLit      :: Int  -> Expr Int
+//      BoolLit     :: Bool -> Expr Bool
+//      Plus        :: Expr Int -> Expr Int -> Expr Int
+//      If          :: Expr Bool -> Expr Y -> Expr Y
+
+
 public interface Visitor<C> {
     <A> App<C, A> caseIntLit (IntLit expr);
     <A> App<C, A> caseBoolLit (BoolLit expr);
