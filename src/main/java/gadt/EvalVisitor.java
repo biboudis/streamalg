7c713a4887ed5a81d52c090bde98fd6c934c556f
Aggelos Biboudis <biboudis@gmail.com>
2014-12-10 17:17:00 +0200

    Add eval to EvalVisitor.

--- src/main/java/gadt/EvalVisitor.java
+++ src/main/java/gadt/EvalVisitor.java
@@ -5,6 +5,10 @@ package gadt;
  */
 public class EvalVisitor implements Visitor<Id.t> {
 
+    <T> Id<T> eval(Expr<T> e){
+        return Id.prj(e.accept(this));
+    }
+
     @Override
     public App<Id.t, Integer> caseIntLit(IntLit expr) {
         return Id.newA(expr.value);
@@ -18,16 +22,16 @@ public class EvalVisitor implements Visitor<Id.t> {
     @Override
     public App<Id.t, Integer> casePlus(Plus expr) {
 
-        Id<Integer> left = Id.prj(expr.left.accept(this));
-        Id<Integer> right = Id.prj(expr.right.accept(this));
+        Id<Integer> left = Id.prj(eval(expr.left));
+        Id<Integer> right = Id.prj(eval(expr.right));
 
         return Id.newA(left.value + right.value);
     }
 
     @Override
     public <T> App<Id.t, T> caseIf(If<T> expr) {
-        Id<Boolean> test = Id.prj(expr.x.accept(this));
+        Id<Boolean> test = Id.prj(eval(expr.x));
 
-        return test.value ? expr.y.accept(this): expr.z.accept(this);
+        return test.value ? eval(expr.y): eval(expr.z);
     }
 }
