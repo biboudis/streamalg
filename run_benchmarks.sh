9200ac3753e9ad5c8ba029d3d07f8fb1d977032c
Aggelos Biboudis <biboudis@gmail.com>
2014-12-18 01:47:48 +0200

    Disable caching on travis.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -11,6 +11,6 @@ echo "Building JMH benchmarks über-jar"
 mvn -DskipTests -q clean package
 
 echo "Running benchmarks"
-java -Xmx2g -Xms2g -XX:-TieredCompilation \
+java -Xmx3g -Xms3g -XX:-TieredCompilation \
      -Dbenchmark.N=1000000 -Dbenchmark.F=3000000 -Dbenchmark.N_outer=1000000 -Dbenchmark.N_inner=10 \
      -jar target/microbenchmarks.jar -wi 5 -i 5 -f 1 -gc true -tu ms ${FILTER}
