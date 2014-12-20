de59f37fb490e9c58298c528edb278fff174e8ee
Aggelos Biboudis <biboudis@gmail.com>
2014-12-20 15:59:36 +0200

    Update script.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -13,4 +13,4 @@ mvn -DskipTests -q clean package
 echo "Running benchmarks"
 java -Xmx3g -Xms3g -XX:-TieredCompilation\
      -Dbenchmark.N=10000000 -Dbenchmark.F=1000000 -Dbenchmark.N_outer=1000000 -Dbenchmark.N_inner=10 \
-     -jar target/microbenchmarks.jar -wi 5 -i 5 -f 3 -gc true -tu ms -rf csv ${FILTER}
+     -jar target/microbenchmarks.jar -wi 5 -i 5 -f 2 -gc true -tu ms -rf csv ${FILTER}
